0095434fc398f7e3527ee218ba45448efad818fc
mcronin79 <mikecronin79@gmail.com>
2020-01-07 13:14:03 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -93,9 +93,9 @@ l1 = layout([[fig, temperature_fig]], sizing_mode='fixed')
 #l1 = gridplot([[temperature_fig, fig], [temperature_fig, fig]], sizing_mode='stretch_both')
 #l2 = gridplot([[temperature_fig, fig], [temperature_fig, fig]], sizing_mode='stretch_both')
 
-#tab1 = Panel(child=l1,title="Air Quality")
+tab1 = Panel(child=l1,title="Air Quality")
 #tab2 = Panel(child=l2,title="Metrics")
-#tabs = Tabs(tabs=[ tab1, tab2 ])
+tabs = Tabs(tabs=[ tab1 ])
 
 curdoc().title = "Hello, world!"
-curdoc().add_root(l1)
+curdoc().add_root(tabs)
