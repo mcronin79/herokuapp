724d049691a202ce4ad3b077613fc173e2b5d738
mcronin79 <mikecronin79@gmail.com>
2020-01-07 12:22:56 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -23,6 +23,66 @@ print(" ")
 print(df.shape)
 print(len(df.index))
 
+
+df.columns = [c.replace(" ","_") for c in df.columns]
+skinned_headers = df.dtypes.index
+
+str_temperature = df['Temperature']
+str_rtd_temperature = df['RTD_Temperature']
+str_humidity = df['Humidity']
+str_weight1 = df['Weight1']
+str_weight2 = df['Weight2']
+str_weight3 = df['Weight3']
+str_weight4 = df['Weight4']
+str_loadcell_1 = df['Load_Cell1']
+str_loadcell_2 = df['Load_Cell2']
+str_loadcell_3 = df['Load_Cell3']
+str_loadcell_4 = df['Load_Cell4']
+str_vusb = df['VUSB']
+str_weight_code = df['Weight_Code']
+str_CO2 = df['CO2']
+
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%d/%m/%Y %H:%M:%S')
+df['Temperature'] = df['Temperature'].astype(float)
+df['RTD_Temperature'] = df['RTD_Temperature'].astype(float)#.apply(lambda x: x - 0.15)
+df['Humidity'] = df['Humidity'].astype(float)
+
+df['O02'] = df['CO2'].astype(int)
+df['Weight_Code'] = df['Weight_Code'].astype(int)
+
+df['Load_Cell1'] = df['Load_Cell1'].astype(float)
+df['Load_Cell2'] = df['Load_Cell2'].astype(float)
+df['Load_Cell3'] = df['Load_Cell3'].astype(float)
+df['Load_Cell4'] = df['Load_Cell4'].astype(float)
+
+df['VUSB'] = df['VUSB'].astype(float)
+
+non_z_weights = df.query('Load_Cell1 != 0 & Load_Cell2 != 0 & Load_Cell3 != 0 & Load_Cell4 != 0')
+print(non_z_weights.head())
+
+# todo: add grid to plot
+time = df['Timestamp']
+temperature = df['Temperature']
+rtd_temperature = df['RTD_Temperature']
+humidity = df['Humidity']
+time_for_weight = non_z_weights['Timestamp'] # drop times where weight is recoded as zero
+
+
+def plot_temperature():
+    p = figure(title="Temperature", title_location="above", x_axis_type='datetime', tools=tools, toolbar_location="above")
+    p.line(time, str_temperature, color='magenta', legend='Temperature')
+    p.line(time, str_rtd_temperature, color='green', legend='RTD_Temperature')
+
+    p.plot_height = 600
+    p.plot_width = 800
+    p.xaxis.axis_label = 'Time'
+    p.yaxis.axis_label = 'Temperature (°C)'
+
+    return p
+
+
+
+
 fig = figure(title='Line plot!', sizing_mode='scale_width')
 fig.line(x=[1, 2, 3], y=[1, 4, 9])
 
