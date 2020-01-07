5ab42927826624f57026f1d03a8022fb82e87dcc
mcronin79 <mikecronin79@gmail.com>
2020-01-07 12:33:03 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -81,10 +81,18 @@ def plot_temperature():
     return p
 
 
-
+temperature_fig = plot_temperature()
 
 fig = figure(title='Line plot!', sizing_mode='scale_width')
 fig.line(x=[1, 2, 3], y=[1, 4, 9])
 
+#l1 = layout([[temperature_fig, load_cell_voltages_fig]], sizing_mode='stretch_both')
+l1 = gridplot([[temperature_fig, fig], [temperature_fig, fig]], sizing_mode='stretch_both')
+l2 = gridplot([[temperature_fig, fig], [temperature_fig, fig]], sizing_mode='stretch_both')
+
+tab1 = Panel(child=l1,title="Air Quality")
+tab2 = Panel(child=l2,title="Metrics")
+tabs = Tabs(tabs=[ tab1, tab2 ])
+
 curdoc().title = "Hello, world!"
-curdoc().add_root(fig)
+curdoc().add_root(tabs)
