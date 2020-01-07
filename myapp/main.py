93d2982438f99966db37d4077fc8419cb26743af
mcronin79 <mikecronin79@gmail.com>
2020-01-07 16:05:45 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -22,7 +22,6 @@ json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
 creds_dict = json.loads(json_creds)
 creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 credentials = ServiceAccountCredentials.from_json_keyfile_name(creds_dict, scope) # Your json file here
 gc = gspread.authorize(credentials)
 wks = gc.open('MyHiveDataSheet').sheet1
