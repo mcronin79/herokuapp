7643c20db8d7439da5451fcbfa0e2090138e0745
mcronin79 <mikecronin79@gmail.com>
2020-01-08 11:33:59 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -433,8 +433,6 @@ def update():
            'color': [random.choice(['red', 'blue', 'green'])]}
     streamsource.stream(new)
 
-doc.add_periodic_callback(update, 100)
-
 fig = figure(title='Streaming Circle Plot!', sizing_mode='scale_width',
              x_range=[0, 1], y_range=[0, 1])
 fig.circle(source=source, x='x', y='y', color='color', size=10)
