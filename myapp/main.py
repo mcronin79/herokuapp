c4bcea2fd84af8f4273d0926939760d11d6bfe9a
mcronin79 <mikecronin79@gmail.com>
2020-01-22 19:31:16 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -251,11 +251,11 @@ temperature_fig_test.yaxis.axis_label = 'Temperature (°C)'
 
 #date_time_str = 'Jun 28 2018  7:40AM'
 #date_time_obj = datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
-N=1
+#N=1
 
 def update():
     global testData
-    global N
+    #global N
     print("GSheetRow.Timestamp.value")
     print(GSheetRow.Timestamp.value)
     
@@ -284,14 +284,16 @@ def update():
         #nuData = {'Timestamp': [date_time_obj],
         #   'Temperature': [testArray[GSheetRow.Temperature.value]]}
         nuData = {'Timestamp': [mydate],
-           'Temperature': [mytemp + N]}
+           'Temperature': [mytemp]}
         testsource.stream(nuData)
         new = {'x': [random.random()],
                'y': [random.random()],
                'color': [random.choice(['red', 'blue', 'green'])]}
         source.stream(new)
-        N = N + 1
-        print('N = ', N)
+        #N = N + 1
+        #print('N = ', N
+        testData = []
+        print("Finished Update")
     #global gsheetRows
     
     ##global testDataFrame
